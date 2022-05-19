92b3f3cfc5d1630550819b95ff5ed96eb134df5a
chauhoaivu111 <chauhoaivu111@gmail.com>
2022-05-19 21:18:23 +0700

    topnav

--- src/components/topnav/TopNav.jsx
+++ src/components/topnav/TopNav.jsx
@@ -1,6 +1,53 @@
 import React from 'react'
 import './TopNav.scss'
 import DropDown from '../dropdown/DropDown'
+import { Link } from 'react-router-dom'
+
+import notifications from '../../assets/JsonData/notification.json';
+import user_image from '../../assets/images/HV.jpg';
+import user_menu from '../../assets/JsonData/user_menus.json';
+
+
+
+
+const curr_user ={
+  display_name:"Hoai Vu",
+  image: user_image
+
+}
+
+const renderNotificationsItem = (item,index) =>(
+  <div className='notification-item' key={index}>
+    <i className={item.icon}></i>
+    <span>{item.content}</span>
+
+
+  </div>
+)
+
+const renderUserToggle =(user) =>(
+
+  <div className='topnav__right-user'>
+    <div className="topnav__right-user__image">
+      <img src ={user.image} alt=''/>
+
+    </div>
+    <div className="topnav__right-user__name">
+      {user.display_name}
+    </div>
+
+  </div>
+)
+
+
+const renderUserMenu = (item,index) => (
+  <Link className='notification-item' key={index}>
+    <i className={item.icon}></i>
+    <span>{item.content}</span>
+
+  </Link>
+)
+
 const TopNav = () => {
   return (
     <div className='topnav'>
@@ -11,16 +58,38 @@ const TopNav = () => {
       </div>
       <div className="topnav__right">
         <div className="topnav__right-item">
-          {/* dropdown here */}
-          fsf
+        <DropDown
+         customToggle={() => renderUserToggle(curr_user)}
+
+        contentData={user_menu}
+         renderItems={(item,index) => renderUserMenu(item,index)}
+          
+
+          
+          />
+         
+         
         </div>
         <div className="topnav__right-item">
-          {/* dropdown here */}
-          sfsf
+          <DropDown
+          icon='bx bx-bell'
+          badge='12'
+           contentData={notifications}
+           renderItems ={(item,index) => renderNotificationsItem(item,index)}
+           renderFooter = {()=> ( 
+
+            <Link to='/'> View All</Link>
+           )}
+
+          
+          />
+         
+          
         </div>
         <div className="topnav__right-item">
           {/* theme setting */}
-          fsf
+          
+         
         </div>
 
       </div>
